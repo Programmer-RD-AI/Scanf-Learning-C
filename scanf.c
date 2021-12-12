f975930956c344ac9fe83f26527b6e8bbc8a883f
Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>
2021-12-12 12:42:23 +0530

    Add files via upload

---
+++ scanf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2;
+    printf(" Enter a number 1 \n - ");
+    scanf("%d", &num1);
+    printf(" Enter a number 2 \n - ");
+    scanf("%d", &num2);
+    printf("%d + %d = %d", num1, num2, num1 + num2);
+}
